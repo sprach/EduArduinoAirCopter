512a7481ff6be8ef8f3d3e1205d4cd85a0952524
sprach <sprach21@daum.net>
2020-06-11 15:12:07 +0900

    day3 files

---
+++ day3/_02motor_order/_02motor_order.ino
@@ -0,0 +1,15 @@
+﻿void setup() {}
+
+void loop() {
+  analogWrite( 6, 10); delay(1000);
+  analogWrite(10, 10); delay(1000);
+  analogWrite( 9, 10); delay(1000);
+  analogWrite( 5, 10); delay(5000);
+  
+  analogWrite( 6, 0);  
+  analogWrite(10, 0);  
+  analogWrite( 9, 0);  
+  analogWrite( 5, 0); delay(2000);
+}
+
+
